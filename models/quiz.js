531cc4b4f30aa6c3fb5a09fc8e612fdc06f39acb
jferna78 <jferna78@gmail.com>
2015-08-16 15:31:10 +0200

    temas

--- models/quiz.js
+++ models/quiz.js
@@ -1,16 +1,31 @@
 // Definicion del modelo de Quiz con validación
 
 module.exports = function(sequelize, DataTypes) {
-  return sequelize.define(
-  	'Quiz',
-    { pregunta: {
-        type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Pregunta"}}
-      },
-      respuesta: {
-        type: DataTypes.STRING,
-        validate: { notEmpty: {msg: "-> Falta Respuesta"}}
-      }
+    return sequelize.define('Quiz', {
+        tema: {
+            type: DataTypes.STRING,
+            validate: { notEmpty: {msg: "-> Falta Tema"},
+                       fn: function(val) {
+                           if (val === "Tema") {throw new Error("-> Falta Tema");}
+                       }
+                      }
+        },
+        pregunta: {
+            type: DataTypes.STRING,
+            validate: { notEmpty: {msg: "-> Falta Pregunta"},
+                       fn: function(val) {
+                           if (val === "Pregunta") {throw new Error("-> Falta Pregunta");}
+                       }
+                      }
+        },
+        respuesta: {
+            type: DataTypes.STRING,
+            validate: { notEmpty: {msg: "-> Falta Respuesta"},
+                       fn: function(val) {
+                           if (val === "Respuesta") {throw new Error("-> Falta Respuesta");}
+                       }
+                      }
+        }
     }
-  );
-}
+                           );
+};
